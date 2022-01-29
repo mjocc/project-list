3c4b295b1b3f8fa391ed71918d3dfcf84e82b6dc
mjocc <61629073+mjocc@users.noreply.github.com>
2022-01-29 23:37:34 +0000

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'bootswatch/dist/darkly/bootstrap.min.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Container } from 'react-bootstrap';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/info.svg" />
+      </Head>
+      <Container className="mt-5 px-5">
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
